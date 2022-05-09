ba70658e0df13ccde0762ec31f52307796253ede
Georg Krause <mail@georg-krause.net>
2022-05-09 18:14:15 +0200

    Make install script work on MacOS

--- install.sh
+++ install.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-download_url=$(curl -L -s https://api.github.com/repos/getdeck/getdeck/releases/latest | grep '"browser_download_url": ".*linux.*"' | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*")
+OS="`uname`"
+case $OS in
+	'Linux')
+		OS=linux
+		echo "Detected Linux! (Congratulations!)"
+		;;
+	'Darwin')
+		OS=darwin
+		echo "Detected MacOS"
+		;;
+	*)
+		echo "No supported platform detected"
+		exit 1
+		;;
+esac
+
+download_url=$(curl -L -s https://api.github.com/repos/getdeck/getdeck/releases/latest | grep '"browser_download_url": ".*'$OS'.*"' | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*")
 file_name=$(echo $download_url | grep -oE '[^/]+$')
 curl -L $download_url -o /tmp/$file_name
 unzip -o /tmp/$file_name -d /tmp/deck
-sudo install -o root -g root -m 0755 /tmp/deck/deck /usr/local/bin/deck
+sudo install -m 0755 /tmp/deck/deck /usr/local/bin/deck
 
 # cleanup 
 rm -rf /tmp/$file_name
