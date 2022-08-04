5b41a9de53be42f7bd7c61aa0a76290cb69f02f9
Christian Busch <christian@blueshoe.de>
2022-08-04 11:41:12 +0200

    fix(#22): install.sh with echo -e

--- install.sh
+++ install.sh
@@ -27,9 +27,9 @@ rm -rf /tmp/$file_name
 rm -rf /tmp/deck
 
 # additional information
-# echo -e "\n---\n"
-# echo -e "🎉 Getdeck has been successfully installed\n"
-# echo -e "🚀 Getting started guide: https://getdeck.dev/docs/deck/getting-started"
-# echo -e "🔧 Intro for Ops: https://getdeck.dev/docs/deck/for-devops/intro"
-# echo -e "💻 Intro for Devs: https://getdeck.dev/docs/deck/for-devs/intro\n"
-# echo -e "❓ Any problems? Feel free to give us feedback: https://github.com/Getdeck/getdeck/issues\n"
+echo -e "\n---\n"
+echo -e "🎉 Getdeck has been successfully installed\n"
+echo -e "🚀 Getting started guide: https://getdeck.dev/docs/deck/getting-started"
+echo -e "🔧 Intro for Ops: https://getdeck.dev/docs/deck/for-devops/intro"
+echo -e "💻 Intro for Devs: https://getdeck.dev/docs/deck/for-devs/intro\n"
+echo -e "❓ Any problems? Feel free to give us feedback: https://github.com/Getdeck/getdeck/issues\n"
